a5cd9f5f39135a9dcb4b6dd9ad521815d8d1b6ce
Jessup Jong <jessupjong@Jessups-MacBook-Pro-3.local>
2023-02-26 12:12:04 -0600

    Uploaded added

---
+++ upload/upload.sh
@@ -0,0 +1,6 @@
+python upload_video.py --file="videos/test1.mov"
+                       --title="Summer vacation in California"
+                       --description="Had fun surfing in Santa Cruz"
+                       --keywords="surfing,Santa Cruz"
+                       --category="22"
+                       --privacyStatus="private"
