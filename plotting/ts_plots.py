44a55902c7ee11a088cf0d21f7f2ac1bab26dc6c
Darren Colby <dscolby17@gmail.com>
2023-03-01 17:56:35 -0600

    Added function for plotting sentiment time series

--- plotting/ts_plots.py
+++ plotting/ts_plots.py
@@ -1,16 +1,46 @@
-import altair as alt
-import vega_datasets
 import pandas as pd
-from scraping.comments import get_comments
+import altair as alt
 
-#df = vega_datasets.data.stocks()
+def plot_comment_ts(df: pd.DataFrame, title: str):
+    """
+    Plot the change in sentiment for a video over time
 
-#def plot_comment_ts(df: pd.DataFrame, x: str, y:str):
-    #alt.Chart(df).mark_line().encode(
-        #x='x',
-        #y="y"
-    #)
+    Parameters:
+        df (pd.DataFrame): A pandas dataframe to plot with columns "date" and "sentiment"
+        title (str): A title for the plot
 
-#plot_comment_ts(df, "date", "price")
+    Returns:
+        An altair Chart object
+    """
+    # The base plot
+    ts_plot = alt.Chart(df).mark_line().encode(
+        alt.X(f"yearmonthdatehours(date):T", 
+              title="",
 
-get_comments()
+              # Only displays the first and last ticks and labels
+              # Found suggestion on Stack Overflow from user jakevdp
+              # https://stackoverflow.com/questions/59699412/altair-display-all-axis-ticks-but-only-some-tick-labels
+              axis=alt.Axis(tickCount=df.shape[0])),
+        alt.Y("sentiment", 
+              title="Sentiment")
+    ).properties(
+        title=video_title
+    )
+
+    # The caption, which is not a property in the Chart class
+    # Found solution on Stack Overflow by user jakevdp
+    # https://stackoverflow.com/questions/57244390/how-to-add-a-subtitle-to-an-altair-generated-chart
+    caption = alt.Chart(
+        {"values": [{"text": 
+                     "Sentiment ranges from -1 for most negative to +1 for most positive"}]}
+    ).mark_text(align="left").encode(
+        text="text:N"
+    )
+
+    final_plot = alt.vconcat(
+        ts_plot,
+        caption
+    )
+
+    return final_plot
+
