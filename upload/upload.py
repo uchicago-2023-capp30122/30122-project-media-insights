c7f05095ce2b2c73c48f7e0a90bb048b7d917933
Jessup Jong <jessupjong@Jessups-MacBook-Pro-3.local>
2023-03-01 21:39:46 -0600

    Word cloud and upload update

--- upload/upload.py
+++ upload/upload.py
@@ -3,6 +3,7 @@ import os
 import googleapiclient.discovery
 from googleapiclient.http import MediaFileUpload
 
+
 """
     The upload functionality is possible only with OAuth.
 """
@@ -19,29 +20,6 @@ def upload_http():
     response = requests.post(url, data = video)
     print(response.status_code)
     print(response.text)
-
-def upload():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = "AIzaSyBgP4m7PSCyZMn8V_cGnl4z6uAXryUtYFs"
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
-
-    request = youtube.videos().insert(
-        part="snippet",
-        body={},
-
-        media_body=MediaFileUpload("videos/test1.mov")
-    )
-    response = request.execute()
-
-    print(response)
-
 
 if __name__ == "__main__":
     upload()
