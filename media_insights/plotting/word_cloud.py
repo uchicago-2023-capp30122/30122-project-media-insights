82f73d79a1a8e4b9f9c3cada0cff5e0416d6a2a7
Darren Colby <dscolby17@gmail.com>
2023-03-02 16:10:46 -0600

    Converted to plot_wordcloud function

--- media_insights/plotting/word_cloud.py
+++ media_insights/plotting/word_cloud.py
@@ -4,49 +4,51 @@
 
 import matplotlib.pyplot as plt
 import json
-import pandas as pd
 import numpy as np
 from PIL import Image
 import wordcloud
-from process_comments import regex_fix
 
-stopwords = set(wordcloud.STOPWORDS)
 
-stopwords.update([""])
+def plot_wordcloud():
+    """
+    Plot a word cloud for comments on a channel
 
-with open("../data/preprocessed_comment_data.json", "r") as f:
-    text = json.load(f)
+    Returns:
+        A matplotlib object
+    """
+    with open("../data/preprocessed_comment_data.json", "r") as f:
+        text = json.load(f)
 
-total_text = " ".join(
-    [
-        comment[0]
-        for key in text
-        for comment in text[key]
-        if "Paul" not in comment[0] and "Tony" not in comment[0]
-    ]
-)
+    total_text = " ".join(
+        [
+            comment[0]
+            for key in text
+            for comment in text[key]
+        ]
+    )
 
-total_text = regex_fix(total_text)
-
-for i in range(3):
-    dog_mask = np.array(Image.open(f"../data/dog{i}.png"))
+    dog_mask = np.array(Image.open(f"../data/dog.png"))
 
     DPI = 1000
 
     word_cloud_inst = wordcloud.WordCloud(
         background_color="white",
-        stopwords=stopwords,
         mask=dog_mask,
         width=2000,
         height=2000,
     )
+
     dog_colors = wordcloud.ImageColorGenerator(dog_mask)
     dog_word_cloud = word_cloud_inst.generate(total_text)
 
-    plt.figure(figsize=(10, 10), dpi=DPI)
-    plt.imshow(
+    plot = plt.figure(figsize=(10, 10), dpi=DPI)
+
+    plot.imshow(
         dog_word_cloud.recolor(color_func=dog_colors, random_state=3),
         interpolation="bilinear",
     )
-    plt.axis("off")
-    plt.savefig(f"../data/word_cloud{i}.png", bbox_inches="tight", dpi=DPI)
+
+    plot.axis("off")
+
+    return plot
+
